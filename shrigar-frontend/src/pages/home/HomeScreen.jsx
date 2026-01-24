5f5d8653dcf41579784ea017bdd82ae1ff4268f1
deepak199395 <Deepak19939426@gmail.com>
2026-01-25 00:03:15 +0530

    creating shop by budget

--- shrigar-frontend/src/pages/home/HomeScreen.jsx
+++ shrigar-frontend/src/pages/home/HomeScreen.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Carousel from '../../components/common/Carousel/Carousel'
 import ProdCollections from '../product/Collections/ProdCollections'
 import ProjCategories from '../product/Categories/ProjCategories'
+import ShopByBudget from '../../components/budget/ShopByBudget'
 
 const HomeScreen = () => {
   return (
@@ -9,6 +10,7 @@ const HomeScreen = () => {
       <Carousel/>
       <ProdCollections/>
       <ProjCategories/>
+      <ShopByBudget/>
     </div>
   )
 }
