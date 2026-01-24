187811b11a2d4867c648261edf426085bbc5dcec
deepak199395 <Deepak19939426@gmail.com>
2026-01-24 16:46:09 +0530

    add corosel and build file

--- shrigar-frontend/src/components/layout/Header/Header.jsx
+++ shrigar-frontend/src/components/layout/Header/Header.jsx
@@ -1,24 +1,43 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./Header.css";
-import logo from "../../../assets/images/logo.jpeg"
+import logo from "../../../assets/images/logo.jpeg";
+
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="header">
 
       {/* Logo */}
       <Link to="/" className="logo">
-        <img
-          src={logo}
-          alt="Shrigar Luxury Intimate Jewellery"
-        />
+        <img src={logo} alt="Shrigar Luxury Intimate Jewellery" />
       </Link>
 
-      {/* Navigation */}
-      <nav className="nav">
+      {/* Desktop Nav */}
+      <nav className="nav desktop-nav">
         <Link to="/">Home</Link>
         <Link to="/shop">Shop</Link>
         <Link to="/cart">Cart</Link>
         <Link to="/account">Account</Link>
+      </nav>
+
+      {/* Hamburger */}
+      <div
+        className={`hamburger ${menuOpen ? "open" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Mobile Menu */}
+      <nav className={`mobile-nav ${menuOpen ? "show" : ""}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+        <Link to="/shop" onClick={() => setMenuOpen(false)}>Shop</Link>
+        <Link to="/cart" onClick={() => setMenuOpen(false)}>Cart</Link>
+        <Link to="/account" onClick={() => setMenuOpen(false)}>Account</Link>
       </nav>
 
     </header>
