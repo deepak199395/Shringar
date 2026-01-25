2337e9232278913389edcde457947ea80b7d0efa
deepak199395 <Deepak19939426@gmail.com>
2026-01-25 16:43:02 +0530

    adding azure baseURL

--- shrigar-frontend/src/config/env.jsx
+++ shrigar-frontend/src/config/env.jsx
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://shop999backend.vercel.app/api/v1";
+export const BASE_URL = "https://devdeepak-backend-api-fbdhhyeddwbab9da.centralindia-01.azurewebsites.net/api/v1";
