4cbf48728499d96351620fea4614d2a4cd831fa6
deepak199395 <Deepak19939426@gmail.com>
2026-01-25 08:07:06 +0530

    adding secodary navigations

---
+++ shrigar-frontend/src/config/navigation.config.jsx
@@ -0,0 +1,11 @@
+export const SECONDARY_NAV = [
+  { label: "New Arrivals", path: "/new-arrivals" },
+  { label: "Bestsellers!", path: "/bestsellers" },
+  { label: "Shop Jewellery", path: "/shop" },
+  { label: "Collections", path: "/collections" },
+  { label: "Exclusive Sale", path: "/sale" },
+  { label: "Boutiques", path: "/boutiques" },
+  { label: "Spotted", path: "/spotted" },
+  { label: "Gift Card", path: "/gift-card" },
+  { label: "About Us", path: "/about" }
+];
--- shrigar-frontend/src/pages/home/HomeScreen.jsx
+++ shrigar-frontend/src/pages/home/HomeScreen.jsx
@@ -3,10 +3,12 @@ import Carousel from '../../components/common/Carousel/Carousel'
 import ProdCollections from '../product/Collections/ProdCollections'
 import ProjCategories from '../product/Categories/ProjCategories'
 import ShopByBudget from '../../components/budget/ShopByBudget'
+import SecondaryNav from '../../components/navigationBar/SecondaryNav'
 
 const HomeScreen = () => {
   return (
     <div>
+      <SecondaryNav/>
       <Carousel/>
       <ProdCollections/>
       <ProjCategories/>
