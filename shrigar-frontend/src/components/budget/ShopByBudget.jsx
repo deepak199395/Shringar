e2b40e5b2f35c7ffc1f2af2ffde8da4c0306c9e4
deepak199395 <Deepak19939426@gmail.com>
2026-05-08 22:52:05 +0530

    Integrated Under Budget API in frontend

--- shrigar-frontend/src/components/budget/ShopByBudget.jsx
+++ shrigar-frontend/src/components/budget/ShopByBudget.jsx
@@ -1,18 +1,51 @@
-import { SHOP_BY_BUDGET } from "../../config/budget.config";
+import React, { useEffect, useState } from "react";
 import "./ShopByBudget.css";
+import axios from "axios";
 
 const ShopByBudget = () => {
+  const [budgetData, setBudgetData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // featch api
+  const getUnderBudgetData = async () => {
+    try {
+      setLoading(true);
+
+      const response = await axios.get(
+        "https://api.shrigaar.com/api/v1/shringar/underBudget/list/api79",
+      );
+      if (response.data.success) {
+        setBudgetData(response.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  // 🔥 On Load
+  useEffect(() => {
+    getUnderBudgetData();
+  }, []);
   return (
     <section className="budget-section">
       <h2 className="budget-heading">SHOP BY BUDGET</h2>
 
       <div className="budget-grid">
-        {SHOP_BY_BUDGET.map((item, index) => (
-          <div className="budget-card" key={index}>
-            <span className="budget-label">{item.label}</span>
-            <span className="budget-price">₹ {item.price}</span>
-          </div>
-        ))}
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          budgetData.map((item) => (
+            <div className="budget-card" key={item._id}>
+              {/* Image */}
+              <img src={item.image} alt={item.title} className="budget-image" />
+              {/* Title */}
+              <span className="budget-label">{item.title}</span>
+              {/* Amount */}
+              <span className="budget-price">₹ {item.amount}</span>
+            </div>
+          ))
+        )}
       </div>
     </section>
   );
